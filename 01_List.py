5657d13e3bc12d5a3e01558620cf65ae8c183835
Eric Yang <eric420@gmail.com>
2021-11-14 05:34:39 +0800

    2021/11/14 update
    2021/11/14 update add index for list

--- 01_List.py
+++ 01_List.py
@@ -98,3 +98,26 @@ list_number = [i for i in range(1, 10)]
 print(list_number)
 #output:
 #[1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+#Stetement for dealing with python list
+print("Stetement for dealing with python list")
+list_number = list(range(10))   #generate 0~9 index in list
+print(list_number[:])           #print all elements from list_number
+#output:
+#[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print(list_number[0:3])         #get 3 elements from index 0
+#[0, 1, 2]
+
+print(list_number[1:])          #get all elements after index 1 (include 1)
+#[1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print(list_number[:4])          #get 4 elements from index 0
+#[0, 1, 2, 3]
+
+print(list_number[:-1])         #get all elements before last index - 1
+#[0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+print(list_number[0:8:2])       #From the 2nd element to the 8th element, each element has a space of 2
+#[0, 2, 4, 6]
